c885dc0b405edc219cd474fa7569a2735fe0e47d
Dat Nguyen <dnguyen74@gmail.com>
2018-02-28 07:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ function nowServing () {
   }  
 }
 
+function currentLiine (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    
+  }
+}
+
+
+
