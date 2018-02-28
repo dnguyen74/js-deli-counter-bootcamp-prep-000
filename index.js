e3da8a22de74ff25d582b6c76b735c9ef3e495a7
Dat Nguyen <dnguyen74@gmail.com>
2018-02-28 06:46:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var katzDeli = [];
+
+function takeANumber (katzDeliLine, custName) {
+  //katzDeli = [];
+  katzDeliLine.push(custName);
+  
+  return "Welcome, " + custName + ". You are number " + katzDeliLine.length + " in line.";
+}
+
+function nowServing () {
+  var deliLine = katzDeli;
+  
+  if (katzDeli.length === 0) {
+    return 'There is nobody waiting to be served!'
+  }
+  else {
+    katzDeli.shift();
+    return "Currently serving " + deliLine[0];
+  }
+}
+
